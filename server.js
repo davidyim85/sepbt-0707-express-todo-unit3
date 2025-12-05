be80332f72802c77f318b1d1b6e5985c5cc06032
yimd85 <yimd85@gmail.com>
2025-12-04 21:01:44 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+//all our imports
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors'); 
+const mongoose = require('mongoose');
+
+
+dotenv.config(); 
+const app = express(); 
+
+
+//connection to the db here
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+//
+
+
+//middleware FOR ALL here
+app.use(cors()); //this allows other servers to call our api
+app.use(express.json()); //allows JSON data to be sent to our routes
+//
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`now listen to port: ${process.env.PORT}`)
+})
